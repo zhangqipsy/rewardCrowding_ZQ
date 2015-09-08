9c04d168d66fb0afe367321540c4f1ef3a34b217
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-09-07 21:53:19 -0400

    mkdir by default

--- startup.m
+++ startup.m
@@ -1,3 +1,5 @@
+mkdir data/Demo;
+mkdir data/RewardTask;
 addpath('./data','./data/Demo/','./data/RewardTask','./lib', './lib/misc', './resources');
 format bank;
 set(0,'DefaultFigurePaperPositionMode','auto')
