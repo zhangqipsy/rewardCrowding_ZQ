8a5f95b4cefa2625ea4c120c966518e3f295a0fb
zhangqipsy <1163898863@qq.com>
2015-09-17 12:56:45 +0800

    nostop

--- lib/genRewardSequence.m
+++ lib/genRewardSequence.m
@@ -112,7 +112,7 @@ end
 %	    isHighReward
 Trials(:, 8) = conf.idxHighRewardColor; % see conf.color.targets for idx used
 randSequence = rand(size(Trials, 1),1);
-keyboard
+%keyboard
 Trials(:, 9) = ((randSequence>conf.highRewardLevel) & (Trials(:, 6) == Trials(:, 8))) +((randSequence<(1-conf.highRewardLevel)) & (Trials(:, 6) ~= Trials(:, 8)));
 
 
