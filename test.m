99978a1ef2162da510d11bbc9e9caaf908a8183c
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-10-07 21:19:10 -0400

    let's do one exp

--- test.m
+++ test.m
@@ -51,7 +51,7 @@ end
           conf.distractorShapes = [Inf]; % Inf is circle
           conf.metric.range_r = -1;
           mode.procedureChannel = 'QUEST'; % experiment methods;
-          conf.QUESTparams        = {7 100 3 0.2 0.82 3.5 0.01 0.5}; % columnN,totalTrials,guess,guessSD,pThreshold,beta,delta,gamma
+          conf.QUESTparams        = {7 3 3 0.2 0.82 3.5 0.01 0.5}; % columnN,totalTrials,guess,guessSD,pThreshold,beta,delta,gamma
           conf.repetitions      = conf.QUESTparams{2};
           rewardedLearning(conf, mode)
         otherwise
