8d9eadf7b21b17e777e2a27c869bc1b9c9ca167f
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-10-12 23:21:58 -0400

    godlike hex converter!

--- lib/misc/colorHex2Dec.m
+++ lib/misc/colorHex2Dec.m
@@ -3,5 +3,6 @@ function colorDecCodes = colorHex2Dec(colorHexCodes)
 %    colorHexCodes = '315724'
 %    colorHexCodes = '#315724'
 %    colorHexCodes = ['#315724' '#315724'] 
-colorDecCodes = reshape(hex2dec(cellstr(reshape(strrep(colorHexCodes, '#', '')', 2,[])')), 3, [])';
+%    colorHexCodes = ['#315724 #315724'] % Even this! Just copy paste your stuff
+colorDecCodes = reshape(hex2dec(cellstr(reshape(regexprep(colorHexCodes, '[# ]', '')', 2,[])')), 3, [])';
 end
