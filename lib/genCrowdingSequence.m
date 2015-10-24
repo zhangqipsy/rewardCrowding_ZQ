baf9e284f4aa535cd79c9b371e09ed2377c56b78
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-10-24 04:36:40 -0400

    don't do what you did before! (in a switch already)

--- lib/genCrowdingSequence.m
+++ lib/genCrowdingSequence.m
@@ -172,9 +172,7 @@ case {'QUEST' , 'quest'}
 %	Column 2
 %	    blockID
 Trials(:,2) = blockID;
-if codeProcedure ~= 0 % Constant procedure
 Trials(:, conf.QUESTparams{1}) = codeProcedure;
-end
 
 
 case {'nUp1Down' , 'nup1down'}
