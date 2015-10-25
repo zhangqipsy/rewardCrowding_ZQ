a39d6b5ad2f6882d15b60fd9033c64b8a703214a
zhangqipsy <1163898863@qq.com>
2015-10-25 22:20:26 +0800

    finish the analyzeRewardData.m it is good!

--- lib/analyzeRewardData.m
+++ lib/analyzeRewardData.m
@@ -1,39 +1,28 @@
 function stat = analyzeRewardData(data)
 
+columnTest = 11; % reaction time
+columnCond1 = 9; % 1 for high reward 0 for low reward
+columnCond2 = 13; % 1 for correct 0 for incorrect
 
+stat.allTrialNum = size(data.Trials,1);
+stat.Trials = data.Trials(data.Trials(:,columnTest)>0,:);% no NaN
 
-stat.Trials = data.Trials(data.Trials(:,11)>0,:);
+stat.miss = 1-size(stat.Trials,1)/stat.allTrialNum; %the persentage of miss trials
 
-stat.acc =max(stat.Trials(:,3))/size(stat.Trials,1);
+stat.accAll =max(stat.Trials(:,3))/size(stat.Trials,1);% acc for all trial with keypress
+stat.accHigh =  mean(stat.Trials(stat.Trials(:,columnCond1) ==1,columnCond2));% acc for high reward trials
+stat.accLow =  mean(stat.Trials(stat.Trials(:,columnCond1) ==0,columnCond2));% acc for low reward trials
 
-stat.meanAccHigh =  mean(stat.Trials(stat.Trials(:,9) ==1,13));
-stat.meanAccLow =  mean(stat.Trials(stat.Trials(:,9) ==0,13));
+[stat.meansd(:,1) stat.meansd(:,2)] = grpstats( stat.Trials(:, columnTest), [stat.Trials(:,columnCond1),  stat.Trials(:,columnCond2)], {'mean', 'std'});
+% the sequence is 00-low reward incorrect 01-low reward correct 
+%                 10-high reward incorrec 11-high reward correct
 
-[a b]=grpstats( stat.Trials(:, 11), [stat.Trials(:,9),  stat.Trials(:,13)], {'mean', 'std'})
-
-stat.highRewardC = stat.Trials(stat.Trials(:,9) ==1 & stat.Trials(:,13) ==1,:);
-stat.lowRewardC = stat.Trials(stat.Trials(:,9) ==0 & stat.Trials(:,13) ==1,:);
-stat.highRewardI = stat.Trials(stat.Trials(:,9) ==1 & stat.Trials(:,13) ==0,:);
-stat.lowRewardI = stat.Trials(stat.Trials(:,9) ==0 & stat.Trials(:,13) ==0,:);
-
-stat.meanHighRewardC = mean(stat.highRewardC(:,11));
-stat.stdHighRewardC = std(stat.highRewardC(:,11));
-stat.meanLowRewardC = mean(stat.lowRewardC(:,11));
-stat.stdLowRewardC = std(stat.lowRewardC(:,11));
-
-stat.meanHighRewardI = mean(stat.highRewardI(:,11));
-stat.stdHighRewardI = std(stat.highRewardI(:,11));
-stat.meanLowRewardI = mean(stat.lowRewardI(:,11));
-stat.stdLowRewardI = std(stat.lowRewardI(:,11));
-
-
-stat.rewardResult = [stat.meanHighRewardC stat.stdHighRewardC stat.meanLowRewardC stat.stdLowRewardC;...
-                     stat.meanHighRewardI stat.stdHighRewardI stat.meanLowRewardI stat.stdLowRewardI;];
+stat.Result = [stat.meansd(4,1),stat.meansd(2,1),stat.meansd(4,2),stat.meansd(2,2)];% only correct trials
                  
 
-boxplot( stat.Trials(:, 11), [stat.Trials(:,9),  stat.Trials(:,13)], 'labels',{'low-incorrect', 'low-correct', 'high-incorrect', 'high-correct'})
+boxplot( stat.Trials(:, columnTest), [stat.Trials(:,columnCond1),  stat.Trials(:,columnCond2)], 'labels',{'low-incorrect', 'low-correct', 'high-incorrect', 'high-correct'});
 hold on;
-plot([stat.meanLowRewardI,stat.meanLowRewardC,stat.meanHighRewardI,stat.meanHighRewardC],'d')
+plot(stat.meansd(:,1),'d');
 hold off;
 
 end
