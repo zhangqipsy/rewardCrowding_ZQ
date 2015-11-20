72f467ef555e9240e321cb6e9fca410c28b92059
zhangqipsy <1163898863@qq.com>
2015-11-20 16:36:08 +0800

    change try and test parameters

--- lib/expCrowding.m
+++ lib/expCrowding.m
@@ -23,7 +23,6 @@ function wrkspc = expCrowding(isDemo, once_on, procedureChannel, Constantparams)
     %conf.color.targets = [conf.color.red, conf.color.green]; % this is default
     mode.exclusiveTargetFlankerColor_on = 1;
     conf.deg.cir_r       = 1/2;
-    conf.showTime = [0.05 0.1 0.15 0.2 0.3 0.5 1];
     fromLab.cyan = [4 157 196]; % L=60 c=36 h=-124
     fromLab.orange = [193 130 93]; % L=60 c=36 h=56
     fromLab.purple = [180 127 180];
@@ -38,10 +37,15 @@ function wrkspc = expCrowding(isDemo, once_on, procedureChannel, Constantparams)
     %TRY
     conf.deg.range_r = [0 5];%column7 targetflankerDist
     conf.repetitions = 20;
+    conf.showTime = [1];
+    mode.drawBlackCircle = 1;
     %TEST
     %conf.deg.range_r = [0.9 1.3 1.8 2.5 3.5 5];%test there is crowding %column7 targetflankerDist
     %conf.repetitions = 50;
+    %mode.drawBlackCircle = 0;
+    %conf.showTime = [0.2];
     
+    %conf.showTime = [0.05 0.1 0.15 0.2 0.3 0.5 1];
     %conf.adaptiveColumn = 7; % 5: targetDist, 7: flankerDist
     conf.adaptiveColumn = NaN;
     conf.QUESTparams  = {conf.adaptiveColumn conf.repetitions 0 4 0.82 3.5 0.01 0.5}; % columnN,totalTrials,guess,guessSD,pThreshold,beta,delta,gamma
